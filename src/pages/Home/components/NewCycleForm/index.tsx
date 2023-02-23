63582d9b10a1e5db4d763a87940294c4b95c50a0
lucaslomba <lucasazevedolomba@gmail.com>
2023-02-23 07:14:06 -0300

    Removendo batata

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -23,7 +23,7 @@ export function NewCycleForm() {
       <datalist id="task-suggestions">
         <option value="Projeto 1" />
         <option value="Projeto 2" />
-        <option value="Batata" />
+        <option value="Projeto 3" />
       </datalist>
 
       <label htmlFor="minutesAmount">durante</label>
